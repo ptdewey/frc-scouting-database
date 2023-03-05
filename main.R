c77201ceb3826d9ee58e84566a4bfaad65e3474e
TheSupremeToast <patrickdewey2002@gmail.com>
2023-03-04 21:20:03 -0500

    fixes

--- main.R
+++ main.R
@@ -44,9 +44,9 @@ for (file in files) {
     # clean out unplayed matches
     df <- df[which(df$scores != -1),]
     auto_dock <- length(which(df$auto_dock == 'Docked'))
-    auto_balance <- length(which(df$auto_balance == 'Level'))
+    auto_balance <- length(which(df$auto_balance == 'Level' & df$auto_dock == 'Docked'))
     tele_dock <- length(which(df$tele_dock == 'Docked'))
-    tele_balance <- length(which(df$tele_balance == 'Level'))
+    tele_balance <- length(which(df$tele_balance == 'Level' & df$tele_dock == 'Docked'))
 
     # TODO COUNTS OF DOCKS AND BALANCES
     allteams <- rbind(allteams, c(tkey, 
@@ -55,7 +55,7 @@ for (file in files) {
 }
 cols <- c('team', 'avg_score', 'avg_auto_game_piece_points', 'avg_auto_points', 
     'count_auto_dock', 'count_auto_level', 'avg_tele_game_piece_points', 
-    'avg_tele_points', 'count_tele_dock', 'count_auto_balance')
+    'avg_tele_points', 'count_tele_dock', 'count_tele_balance')
 colnames(allteams) <- cols
 
 write.csv(allteams, 'vabla_allteams.csv')
