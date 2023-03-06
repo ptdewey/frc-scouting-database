09d5e74b5d74c50db7d15a811f5a96fb51713c19
Patrick <57921252+TheSupremeToast@users.noreply.github.com>
2023-03-06 16:49:05 -0500

    Delete update_mdata.sh

--- update_mdata.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-# Get/Update json containing full event match data
-curl -X 'GET' \
-  'https://www.thebluealliance.com/api/v3/team/frc2106/event/2023vabla/matches' \
-  -H 'accept: application/json' \
-  -H 'X-TBA-Auth-Key: Fot6bq8bTn0rRRiYAxS1a9kSG3yDhDOq03cn50G54MmRQfT448faHNGokaHIrhli' > data/vabla_matches.json
-
