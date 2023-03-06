8f90e391c2d60a8c2cffcd9b40074413ca775283
TheSupremeToast <patrickdewey2002@gmail.com>
2023-03-06 16:33:51 -0500

    event means script

---
+++ eventmean.R
@@ -0,0 +1,19 @@
+getEventMeans <- function(allteams, df, team_key, event_key) {
+    # create team csv file
+    write.csv(df, glue("{event_key}/{event_key}_{team_key}.csv"))
+    # clean out unplayed matches
+    df <- df[which(df$scores != -1),]
+    auto_dock <- length(which(df$auto_dock == 'Docked'))
+    auto_balance <- length(which(df$auto_balance == 'Level' & df$auto_dock == 'Docked'))
+    tele_dock <- length(which(df$tele_dock == 'Docked'))
+    tele_balance <- length(which(df$tele_balance == 'Level' & df$tele_dock == 'Docked'))
+
+    allteams <- rbind(allteams, c(tkey, 
+        mean(as.numeric(df$scores)), mean(as.numeric(df$auto_gpp)), 
+        mean(as.numeric(df$auto_p)), auto_dock, auto_balance, 
+        mean(as.numeric(df$tele_gpp)), mean(as.numeric(df$tele_p)), 
+        tele_dock, tele_balance))
+
+    return(allteams)
+}
+
