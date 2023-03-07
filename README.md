bc8682ce6947bd0d819172f4c6fc2b43dc37e3c0
TheSupremeToast <patrickdewey2002@gmail.com>
2023-03-07 17:07:03 -0500

    tweaks to output structure

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Welcome to a project I made to help with scouting for the 2023 First Robotics Co
 
 1. Clone repository
 2. Install [Dependencies](#Dependencies)
-3. Run ```Rscript main.R``` from the command line or run it in [RStudio](https://posit.co/downloads/) 
+3. Run ```Rscript main.R``` from the command line or in [RStudio](https://posit.co/downloads/) 
 4. Your output csv files can be found within the `output/{event_name}` directory
 
 ## Dependencies:
--- eventmean.R
+++ eventmean.R
@@ -1,6 +1,6 @@
 getEventMeans <- function(allteams, df, team_key, event_key) {
     # create team csv file
-    write.csv(df, glue("{event_key}/{event_key}_{team_key}.csv"))
+    write.csv(df, glue("output/{event_key}/{event_key}_{team_key}.csv"))
     # clean out unplayed matches
     df <- df[which(df$scores != -1),]
     auto_dock <- length(which(df$auto_dock == 'Docked'))
@@ -13,6 +13,11 @@ getEventMeans <- function(allteams, df, team_key, event_key) {
         mean(as.numeric(df$auto_p)), auto_dock, auto_balance, 
         mean(as.numeric(df$tele_gpp)), mean(as.numeric(df$tele_p)), 
         tele_dock, tele_balance))
+
+    cols <- c('team', 'avg_score', 'avg_auto_game_piece_points', 'avg_auto_points', 
+        'count_auto_dock', 'count_auto_level', 'avg_tele_game_piece_points', 
+        'avg_tele_points', 'count_tele_dock', 'count_tele_balance')
+    colnames(allteams) <- cols
 
     return(allteams)
 }
