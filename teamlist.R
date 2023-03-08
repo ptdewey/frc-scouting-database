6980e419675860a5937c022cc0fe3254fe9dee56
Patrick <57921252+TheSupremeToast@users.noreply.github.com>
2023-03-08 15:22:40 -0500

    refactor: updated event-wide output to contain opr scores, added more fields (#1)

--- teamlist.R
+++ teamlist.R
@@ -3,7 +3,7 @@
 # @input api_key: blue alliance api key found on account dashboard
 getTeamList <- function(event_key, api_key) {
     link <- glue("'https://www.thebluealliance.com/api/v3/event/{event_key}/teams'")
-    args <- glue("curl -X 'GET' {link} -H 'accept: application/json' -H 'X-TBA-Auth-Key: {api_key}'")
-    return((system(args, intern=T) %>% fromJSON())$key)
+    cmd <- glue("curl -X 'GET' {link} -H 'accept: application/json' -H 'X-TBA-Auth-Key: {api_key}'")
+    return((system(cmd, intern=T) %>% fromJSON())$key)
 }
 
--- teamopr.R
+++ teamopr.R
@@ -14,6 +14,8 @@ getOpr <- function(df, event_teams) {
     score <- c(df$r_score, df$b_score)
     auto_score <- c(df$r_auto_score, df$b_auto_score)
     tele_score <- c(df$r_teleop_score, df$b_teleop_score)
+    auto_gpc <- c(df$r_auto_gpc, df$b_auto_gpc)
+    tele_gpc <- c(df$r_tele_gpc, df$b_tele_gpc)
 
     # create design matrix
     X <- matrix(0, nrow=length(df$match_number)*2, ncol = length(event_teams))
@@ -26,9 +28,14 @@ getOpr <- function(df, event_teams) {
     auto_opr <- solve(t(X) %*% X) %*% (t(X) %*% auto_score)
     tele_opr <- solve(t(X) %*% X) %*% (t(X) %*% tele_score)
     auto_ratio <- auto_opr / opr
+    auto_gp_opr <- solve(t(X) %*% X) %*% (t(X) %*% auto_gpc)
+    tele_gp_opr <- solve(t(X) %*% X) %*% (t(X) %*% tele_gpc)
 
-    team_contrib <- data.frame(event_teams, opr, auto_opr, tele_opr, auto_ratio)
-    colnames(team_contrib) <- c('team', 'opr', 'auto_opr', 'teleop_opr', 'auto_opr_%')
+
+    team_contrib <- data.frame(event_teams, opr, auto_opr, tele_opr, auto_ratio,
+        auto_gp_opr, tele_gp_opr)
+    colnames(team_contrib) <- c('team', 'opr', 'auto_opr', 'teleop_opr', 
+        'auto_opr_ratio', 'auto_gpc_opr', 'tele_gpc_opr')
     return(team_contrib %>% arrange(desc(opr)))
 }
 
