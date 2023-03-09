2cc8ca9392f43b41f4a4594bf5f1e5c4317b13b4
Patrick <57921252+TheSupremeToast@users.noreply.github.com>
2023-03-09 15:17:34 -0500

    refactor: reduced number of api requests when running (#2)
    * tweaks and more output
    
    * changed event dataframe metric to opr scores for better accuracy
    
    * removed old code
    
    * refactor to reduce number of api requests per run
    
    * removed opr spreadsheet, moved to all

--- eventmatches.R
+++ eventmatches.R
@@ -1,7 +1,7 @@
-# create dataframe containing all matches from event
-# @input event_key: TBA event key
-# @input api_key: TBA api key
-getEventMatches <- function(event_key, api_key) {
+# get all matches from event, raw output form, json schema
+# @input event_key: tba event key 
+# @input api_key: tba api key
+getEventMatchesRaw <- function(event_key, api_key) {
     link <- glue("'https://www.thebluealliance.com/api/v3/event/{event_key}/matches'")
     cmd <- glue("curl -X 'GET' {link} -H 'accept: application/json' -H 'X-TBA-Auth-Key: {api_key}'")
     oldw <- getOption("warn")
@@ -11,7 +11,12 @@ getEventMatches <- function(event_key, api_key) {
         arrange(match_number, .by_group=TRUE) %>%
         as.data.frame()
     options(warn = oldw)
+    return(df)
+}
 
+# create dataframe containing all matches from event
+# @input df: raw event matches dataframe
+getEventMatches <- function(df) {
     # create output dataframe
     out <- data.frame(df$comp_level, df$match_number, df$alliances$red$score, 
         df$score_breakdown$red$autoPoints, df$score_breakdown$red$teleopPoints, 
@@ -30,4 +35,3 @@ getEventMatches <- function(event_key, api_key) {
 
     return(out)
 }
-
